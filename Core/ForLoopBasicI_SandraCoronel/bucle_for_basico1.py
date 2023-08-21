9896d33328b1561d16bd96be97703051cf0fa7c4
Sandra <128000308+SandraCoronel08@users.noreply.github.com>
2023-08-20 20:52:55 -0400

    Update bucle_for_basico1.py

--- Core/ForLoopBasicI_SandraCoronel/bucle_for_basico1.py
+++ Core/ForLoopBasicI_SandraCoronel/bucle_for_basico1.py
@@ -32,3 +32,15 @@ mult = 3
 for num in range(lowNum, highNum + 1):
     if num % mult == 0:
         print(num)
+
+
+x= [ [5,2,3], [10,8,9] ]
+estudiantes = [
+    {'first_name': 'Michael', 'last_name' : 'Jordan'},
+    {'first_name': 'Johm', 'last_name' : 'Rosales'}
+]
+directorio_deportes = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'futbol' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20}]
