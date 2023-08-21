4bbca9ce66580556e3b94a58f9fb525ca84003cf
Sandra <128000308+SandraCoronel08@users.noreply.github.com>
2023-08-20 20:53:16 -0400

    Update funciones3.py

--- Core/functionsIntermediateI_SandraCoronel/funciones3.py
+++ Core/functionsIntermediateI_SandraCoronel/funciones3.py
@@ -1,19 +1,17 @@
-# Obtener valores de una lista de diccionarios
+#3. Obtener valores de una lista de diccionarios
 
 def iterateDictionary2(key_name, some_list):
     for diccionario in some_list:
         if key_name in diccionario:
             print(diccionario[key_name])
 
-# Lista de diccionarios de ejemplo
 estudiantes = [
     {'first_name': 'Michael', 'last_name': 'Jordan'},
     {'first_name': 'John', 'last_name': 'Rosales'},
     {'first_name': 'Mark', 'last_name': 'Guillen'},
     {'first_name': 'KB', 'last_name': 'Tonel'}
 ]
-
-# Llamar a la función iterateDictionary2 con diferentes claves
+#
 print("Resultados para 'first_name':")
 iterateDictionary2('first_name', estudiantes)
 
